fa0b7cc0b49a987741132e627625056c4988509e
Sotatek-DatLuu <datl.luu@sotatek.com>
2023-12-01 11:01:53 +0700

    update response create tenant

--- src/main/java/com/learning/resource/TenantController.java
+++ src/main/java/com/learning/resource/TenantController.java
@@ -1,8 +1,10 @@
 package com.learning.resource;
 
+import com.learning.base.dto.ServerResponse;
 import com.learning.dto.TenantDTO;
 import com.learning.service.TenantDomain;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,7 @@ public class TenantController {
     private final TenantDomain tenantDomain;
 
     @PutMapping
-    public void createTenant(@RequestBody TenantDTO dto) {
-        tenantDomain.createNewTenant(dto);
+    public ResponseEntity<ServerResponse> createTenant(@RequestBody TenantDTO dto) {
+        return ResponseEntity.ok(ServerResponse.successWith(tenantDomain.createNewTenant(dto)));
     }
 }
